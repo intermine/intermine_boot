0cefccfe9a33ce779cb0c78a4ca2d4a43d1e4f9c
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 18:00:31 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -3,7 +3,7 @@ import os
 import time
 import sys
 
-Timeout = 9000
+Timeout = 900
 
 def linux_test():
 	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -26,44 +26,49 @@ def linux_test():
 	print("This test was successful")
 
 def windows_test():
-	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	print("started local intermine")
-	start = time.time()
+	print("Not Supported in Travis")
+	sys.exit(1)
+# 	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# 	print("started local intermine")
+# 	start = time.time()
 	
-	while True:
-		time.sleep(1)
-		end = time.time()
-		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-		if(curl_out.stdout!=''):
-			print(curl_out.stdout)
-			break
-		if((end-start)>Timeout):
-			local_start_out.kill()
-			subprocess.run(["intermine_boot","stop","local"])
-			sys.exit(1)
+# 	while True:
+# 		time.sleep(1)
+# 		end = time.time()
+# 		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+# 		if(curl_out.stdout!=''):
+# 			print(curl_out.stdout)
+# 			break
+# 		if((end-start)>Timeout):
+# 			local_start_out.kill()
+# 			subprocess.run(["intermine_boot","stop","local"])
+# 			sys.exit(1)
 	
-	subprocess.run(["intermine_boot","stop","local"])
-	print("This test was successful")
+# 	subprocess.run(["intermine_boot","stop","local"])
+# 	print("This test was successful")
+
 
 def osx_test():
-	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	print("started local intermine")
-	start = time.time()
+	print("Not Supported in Travis")
+	sys.exit(1)
+# 	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# 	print("started local intermine")
+# 	start = time.time()
 	
-	while True:
-		time.sleep(1)
-		end = time.time()
-		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-		if(curl_out.stdout!=''):
-			print(curl_out.stdout)
-			break
-		if((end-start)>Timeout):
-			local_start_out.kill()
-			subprocess.run(["intermine_boot","stop","local"])
-			sys.exit(1)
+# 	while True:
+# 		time.sleep(1)
+# 		end = time.time()
+# 		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+# 		if(curl_out.stdout!=''):
+# 			print(curl_out.stdout)
+# 			break
+# 		if((end-start)>Timeout):
+# 			local_start_out.kill()
+# 			subprocess.run(["intermine_boot","stop","local"])
+# 			sys.exit(1)
 	
-	subprocess.run(["intermine_boot","stop","local"])
-	print("This test was successful")
+# 	subprocess.run(["intermine_boot","stop","local"])
+# 	print("This test was successful")
 
 os_name = sys.platform
 print("You are using the following operating system:", os_name)
@@ -71,3 +76,6 @@ if(os_name=="linux2" or "linux"):
 	linux_test()
 elif (os_name=="win32"):
 	windows_test()
+elif (os_name == "osx"):
+	osx_test()
+
