0e12e7fc0d757a97dc0579a52431d2dfcbb2036e
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 16:20:25 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -7,9 +7,11 @@ print("started local intermine")
 
 while True:
 	time.sleep(1)
-	curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout = 1000)
-	print(curl_out.stdout)
-	if(curl_out.stdout!=''):
+	try:
+		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout = 1000)
+		print(curl_out.stdout)
+		if(curl_out.stdout!=''):
 		break
-
+	except subprocess.TimeoutExpired:
+		 print('process ran too long')
 subprocess.run(["intermine_boot","stop","local"])
