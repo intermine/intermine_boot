d622854b7475ce7d5831d60f34292a2aeae76232
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 16:06:34 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -7,7 +7,7 @@ print("started local intermine")
 
 while True:
 	time.sleep(1)
-	curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+	curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout = 1000)
 	print(curl_out.stdout)
 	if(curl_out.stdout!=''):
 		break
