1c6ce4abc5920d6fa71d26bb779c76cac0908c2d
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 17:49:43 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -1,17 +1,73 @@
 import subprocess
 import os
 import time
+import sys
 
-local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-print("started local intermine")
+Timeout = 9000
 
-while True:
-	time.sleep(1)
-	try:
-		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout = 1000)
-		print(curl_out.stdout)
+def linux_test():
+	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print("started local intermine")
+	start = time.time()
+	
+	while True:
+		time.sleep(1)
+		end = time.time()
+		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 		if(curl_out.stdout!=''):
+			print(curl_out.stdout)
 			break
-	except subprocess.TimeoutExpired:
-		 print('process ran too long')
-subprocess.run(["intermine_boot","stop","local"])
+		if((end-start)>Timeout):
+			local_start_out.kill()
+			subprocess.run(["intermine_boot","stop","local"])
+			sys.exit(1)
+	
+	subprocess.run(["intermine_boot","stop","local"])
+	print("This test was successful")
+
+def windows_test():
+	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print("started local intermine")
+	start = time.time()
+	
+	while True:
+		time.sleep(1)
+		end = time.time()
+		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		if(curl_out.stdout!=''):
+			print(curl_out.stdout)
+			break
+		if((end-start)>Timeout):
+			local_start_out.kill()
+			subprocess.run(["intermine_boot","stop","local"])
+			sys.exit(1)
+	
+	subprocess.run(["intermine_boot","stop","local"])
+	print("This test was successful")
+
+def osx_test():
+	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print("started local intermine")
+	start = time.time()
+	
+	while True:
+		time.sleep(1)
+		end = time.time()
+		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		if(curl_out.stdout!=''):
+			print(curl_out.stdout)
+			break
+		if((end-start)>Timeout):
+			local_start_out.kill()
+			subprocess.run(["intermine_boot","stop","local"])
+			sys.exit(1)
+	
+	subprocess.run(["intermine_boot","stop","local"])
+	print("This test was successful")
+
+os_name = sys.platform
+print("You are using the following operating system:", os_name)
+if(os_name=="linux2" or "linux"):
+	linux_test()
+elif (os_name=="win32"):
+	windows_test()
