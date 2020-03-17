43b122bdf921f9ad082ca8f2dccec7032fc775b2
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 16:22:36 +0530

    Update test1.py

--- test/test1.py
+++ test/test1.py
@@ -11,7 +11,7 @@ while True:
 		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, timeout = 1000)
 		print(curl_out.stdout)
 		if(curl_out.stdout!=''):
-		break
+			break
 	except subprocess.TimeoutExpired:
 		 print('process ran too long')
 subprocess.run(["intermine_boot","stop","local"])
