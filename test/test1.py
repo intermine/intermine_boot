1e697becd6c3731f8934127339df67cd1b06e950
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-17 15:45:18 +0530

    Create test1.py

---
+++ test/test1.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+import time
+
+local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+print("started local intermine")
+
+while True:
+	time.sleep(1)
+	curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+	print(curl_out.stdout)
+	if(curl_out.stdout!=''):
+		break
+
+subprocess.run(["intermine_boot","stop","local"])
