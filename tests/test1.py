5c692a6811ddb8a5f27998fd8a184add7fe343a6
Nivedita Rufus <42349427+niveditarufus@users.noreply.github.com>
2020-03-31 20:54:08 +0530

    created test for linux

---
+++ tests/test1.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+import time
+import sys
+
+Timeout = 540
+
+def linux_test():
+	local_start_out = subprocess.Popen(["intermine_boot", "start", "local"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	print("started local intermine")
+	start = time.time()
+	
+	while True:
+		time.sleep(1)
+		end = time.time()
+		curl_out = subprocess.run(["curl", "http://localhost:9999/biotestmine/service/version"], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+		if(curl_out.stdout!=''):
+			break
+		if((end-start)>Timeout):
+			local_start_out.kill()
+			subprocess.run(["intermine_boot","stop","local"])
+			print("process ran for too long")
+			sys.exit(1)
+	
+	subprocess.run(["intermine_boot","stop","local"])
+	print("This test was successful")
+
+os_name = sys.platform
+print("You are using the following operating system:", os_name)
+
+if(os_name=="linux2" or "linux"):
+	linux_test()
+else:
+	print("This is not a supported operating system in this test script")
