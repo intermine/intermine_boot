3dc0edea4b4c0ab4083fe966c8c27e65e3264bbd
22PoojaGaur <poojagaur2293@gmail.com>
2020-08-10 16:59:44 +0530

    added debug statement

--- intermine_boot/intermine_docker.py
+++ intermine_boot/intermine_docker.py
@@ -111,6 +111,9 @@ def up(options, env):
         img_path = _get_container_path()
         print ('CONTAINER PATH IS')
         print (_get_container_path())
+        print ('PATH TOMCAT CONTAINER')
+        print (str(img_path / 'tomcat'))
+        print (os.listdir(str(img_path / 'tomcat')))
         tomcat_image = client.images.build(
             path=str(img_path / 'tomcat'), tag='tomcat', dockerfile='tomcat.Dockerfile')[0]
         solr_image = client.images.build(
