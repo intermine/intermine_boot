c4ca4dbc5f57e05894456de66598902176c92ae2
uosl <regen@anche.no>
2021-06-02 17:32:03 +0100

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='intermine_boot',
-    version='0.0.2',
+    version='0.1.0',
     license='LGPL',
     python_requires='>=3.5',
     packages=find_packages(),
